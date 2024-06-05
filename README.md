74ac3491b14a8c5a518938ce5d61248b08ac0ae3
Axel Chepanski <axelchepanski@hotmail.com>
2024-06-05 11:27:03 -0300

    Adicionar todos os arquivos ao repositório

---
+++ README.md
@@ -0,0 +1,23 @@
+# OpenAI: LangChain
+
+## ⚙️ Configuração do Ambiente
+
+### Criando e Ativando o Ambiente Virtual
+
+**Windows:**
+```bash
+python -m venv langchain
+langchain\Scripts\activate
+```
+
+**Mac/Linux:**
+```bash
+python3 -m venv langchain
+source langchain/bin/activate
+```
+
+### Instalação das Bibliotecas
+
+```bash
+pip install -r requirements.txt
+```
---
+++ main.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+cliente  = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+numero_de_dias = 7
+numero_de_criancas = 2
+atividade = "praia"
+
+prompt = f"Crie um roteiro de viagem de {numero_de_dias} dias, para uma família com {numero_de_criancas} crianças, que gostam de {atividade}."
+print(prompt)
+
+resposta = cliente.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt}
+    ]
+)
+
+print(resposta)
+
+roteiro_viagem = resposta.choices[0].message.content
+print(roteiro_viagem)
